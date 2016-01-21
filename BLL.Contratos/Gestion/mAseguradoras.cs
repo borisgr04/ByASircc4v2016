eec36537e9f82da263591278de7a95a0f760b189
Carlos Tirado <orley333@gmail.com>
2016-01-20 21:25:53 -0500

    first commit

---
+++ BLL.Contratos/Gestion/mAseguradoras.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Entidades;
+using Entidades.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL.Model;
+
+namespace BLL.Contratos.Gestion
+{
+    public class mAseguradoras:absBLL
+    {
+        public mAseguradoras()
+        {
+            Mapper.CreateMap<ASEGURADORAS, vAseguradoras>();
+            Mapper.CreateMap<vAseguradoras, ASEGURADORAS>(); 
+        }
+        public List<vAseguradoras> GetAseguradoras()
+        {
+            List<vAseguradoras> List = new List<vAseguradoras>();
+            using (ctx = new Entities())
+            {
+
+                var query = ctx.ASEGURADORAS.ToList();
+                Mapper.Map(query, List);
+
+
+            }
+            return List;
+
+        }
+    }
+}
---
+++ BLL/EntidadBLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ByA;
+using Entidades;
+using DAL.Model;
+
+namespace BLL
+{
+    public class EntidadBLL
+    {
+        public Entities ctx { get; set; }
+        public ByARpt byaRpt { get; set; }
+
+        public byte[] GetLogo()
+        {
+            using (ctx = new Entities())
+            {
+                byte[] logo = ctx.CTRL_ENTIDAD.Select(t => t.LOGO_RPT).Single();
+                return logo;
+            }
+        }
+
+    }
+}
