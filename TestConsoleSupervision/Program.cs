60c4a8a729b93820ba209fc70a565a504a185260
Carlos Tirado <orley333@gmail.com>
2016-01-21 11:31:51 -0500

    first commit

---
+++ TestConsoleSupervision/Program.cs
@@ -0,0 +1,22 @@
+﻿//using TestConsoleSupervision.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model;
+
+namespace TestConsoleSupervision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Entities db = new Entities()) {
+                var q = db.INT_INFOCONT.ToList();
+                Console.WriteLine("Hola "+ q.Count());
+                Console.ReadKey();
+            }
+        }
+    }
+}
